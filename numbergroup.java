b92468c73872d75d62346e711c1817963f079f23
Precious Kindo <lilonaony@gmail.com>
2016-11-21 18:43:58 +0000

    simple even number interator in java

---
+++ numbergroup.java
@@ -0,0 +1,21 @@
+//
+// numbergroup.java
+//
+public class numbergroup
+{
+
+	public static void main(String[] args) throws InterruptedException
+	{
+		public int odd(int x)
+		{
+			if(x%2==0) return true;
+		}
+		int counter = 0;
+		while(counter<20)
+		{
+			System.out.println(counter);
+			counter++;
+		}
+
+	}
+}
